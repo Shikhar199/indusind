99e2a40f6d4cc5d1abd4c2d00f04bc11b93bf91c
Shubham Sharma <shubham.sharma@axeno.co>
2025-01-18 23:57:31 +0530

    Press Wrap Block Update

--- blocks/press-wrap/press-wrap.js
+++ blocks/press-wrap/press-wrap.js
@@ -3,15 +3,13 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {	
     //Fetch the block heading element and set its classes and attributes
-    const blockHeading = document.querySelector('div.press-wrap-container h4');
-    blockHeading.className = "text-bold text-primary";
+    const blockHeading = document.querySelector('div.press-wrap-container h4').textContent;
 
     //Fetch first column data of the row
     const firstColumn = row.children[0];
 
     //Fetch first column's heading element and set its classes and attributes
-    const firstColumnHeading = firstColumn.querySelector('h5');
-    firstColumnHeading.className = "text-primary text-bold";
+    const firstColumnHeading = firstColumn.querySelector('h5').textContent;
 
     //Creating outer divs of News Items
     const latestNewsDiv = document.createElement("div");
@@ -20,17 +18,32 @@ export default function decorate(block) {
     //Fetch list items and set its structure
     const newsItems = firstColumn.querySelectorAll("li");
     newsItems.forEach(newsItem => {
-      const dateText = newsItem.textContent;
+      const newsContainer = document.createElement("div");
+      newsContainer.className = "post border-bottom mb-3 border-primary";
+
+      const postBodyDiv = document.createElement("div");
+      postBodyDiv.className = "post-body pb-3";
+
+      const mb2Div = document.createElement("div");
+      mb2Div.className = "mb-2";
+
+      const postDateSpan = document.createElement("span");
+      postDateSpan.className = "post-date text-primary";
+
       const anchorElement = newsItem.querySelector("a");
 
-      const newsContainer = `
-        <div class="post border-bottom mb-3 border-primary">
-          <div class="post-body pb-3">
-            <div class="mb-2"><span class="post-date text-primary">${dateText}</span></div>
-            <h6 class="post-title three-lines">${anchorElement}</h6>
-          </div>
-        </div>
-      `;
+      const h6 = document.createElement("h6");
+      h6.className = "post-title three-lines";
+      h6.appendChild(anchorElement);
+
+      const dateText = newsItem.textContent;
+      postDateSpan.textContent = dateText;
+
+      mb2Div.appendChild(postDateSpan);
+      postBodyDiv.appendChild(mb2Div);
+      postBodyDiv.appendChild(h6);
+      newsContainer.appendChild(postBodyDiv);
+
       latestNewsDiv.insertAdjacentHTML("beforeend", newsContainer);
     });
 
@@ -40,11 +53,9 @@ export default function decorate(block) {
     bottomAnchorElement1.innerHTML = `
       <span class="btn-inner--icon"><i class="far fa-angle-right"></i></span>
     `;
-    const pressLinkDiv = `
-      <div class="press-link mt-5">
-        ${bottomAnchorElement1}
-      </div>
-    `;
+    const pressLinkDiv = document.createElement("div");
+    pressLinkDiv.className = "press-link mt-5";
+    pressLinkDiv.appendChild(bottomAnchorElement1);
 
 //    latestNewsDiv.insertAdjacentElement('afterend', pressLinkDiv);
 
@@ -57,7 +68,7 @@ export default function decorate(block) {
             </div>
             <div class="container">
                <div class="heading mb-4">
-                  ${blockHeading}
+                  <h4 class="text-bold text-primary">${blockHeading}</h4>
                </div>
                <div class="row">
                   <div class="col-12 col-sm-3">
@@ -67,9 +78,8 @@ export default function decorate(block) {
                               <div id="article-listing-" class="article-listing ">
                                  <div class="col-md-12 mb-5 px-0 aos-init aos-animate" data-aos="fade-up">
                                     <div class="mb-2">
-                                       ${firstColumnHeading}
+                                       <h5 class="text-primary text-bold">${firstColumnHeading}</h5>
                                     </div>
-                                    ${latestNewsDiv}
                                     ${pressLinkDiv}
                                  </div>
                               </div>
@@ -81,58 +91,48 @@ export default function decorate(block) {
             </div>
          </section>
     `;
+    mb2Div.insertAdjacentElement("beforeend", latestNewsDiv);
+    mb2Div.insertAdjacentElement("beforeend", pressLinkDiv);
 
     //Fetch Second Columns' Data from table
     const secondColumn = row.children[1];
 
     //Fetch first column's heading, image, title, para and CTA and set their classes and attributes
-    const secondColumnHeading = secondColumn.querySelector('h5');
-    secondColumnHeading.className = "text-primary text-bold";
+    const secondColumnHeading = secondColumn.querySelector('h5').textContent;
 
-    const secondColumnImage = secondColumn.querySelector('picture img');
-    secondColumnImage.className = "card-border-color dummy-img";
-    const postTitle = secondColumn.querySelector('h4');
-    postTitle.className = "post-title";
+    const secondColumnImage = secondColumn.querySelector('picture img').src;
+    const postTitle = secondColumn.querySelector('h4').textContent;
     const paraText = secondColumn.querySelector('h4').nextElementSibling.textContent;
     const bottomAnchorElement2 = secondColumn.querySelector('.button-container a');
-    bottomAnchorElement2.className = "link_view"
+    const bottomAnchorElement2Href = bottomAnchorElement2.getAttribute("href");
+    const bottomAnchorElement2Text = bottomAnchorElement2.textContent;
 
     //Fetch Third Columns' Data from table
     const thirdColumn = row.children[2];
 
     //Fetch headings, image, title, para and CTA and set their classes and attributes
     const thirdColumnHeadings = thirdColumn.querySelectorAll('h5');
-    thirdColumnHeadings.forEach(heading => {
-      heading.className = "text-primary text-bold";
-    });
-    const thirdColumnHeading1 = thirdColumnHeadings[0];
-    const thirdColumnHeading2 = thirdColumnHeadings[1];
+    const thirdColumnHeading1 = thirdColumnHeadings[0].textContent;
+    const thirdColumnHeading2 = thirdColumnHeadings[1].textContent;
 
     //Fetch images and set their classes and attributes
     const thirdColumnImages = thirdColumn.querySelectorAll('picture img');
-    thirdColumnImages.forEach((image, i) => {
-      image.className = "card-border-color";
-      image.setAttribute("alt", thirdColumnHeadings[i].textContent.toLowerCase());
-    });
-    const image1 = thirdColumnImages[0];
-    const image2 = thirdColumnImages[1];
+    const image1Src = thirdColumnImages[0].src;
+    const image2Src = thirdColumnImages[1].src;
+    const image1Alt = thirdColumnHeading1.toLowerCase();
+    const image2Alt = thirdColumnHeading2.toLowerCase();
 
     //Fetch title headings and set their classes and attributes
     const postTitleHeadings = thirdColumn.querySelectorAll('h4');
-    postTitleHeadings.forEach(postTitleHeading => {
-      postTitleHeading.className = "post-title";
-    });
-    const postTitleHeading1 = postTitleHeadings[0];
-    const postTitleHeading2 = postTitleHeadings[1];
+    const postTitleHeading1 = postTitleHeadings[0].textContent;
+    const postTitleHeading2 = postTitleHeadings[1].textContent;
 
     //Fetch anchor elements and set their classes and attributes
     const anchorElements = thirdColumn.querySelectorAll('.button-container a');
-    anchorElements.forEach(anchorElement => {
-      anchorElement.className = "link_view";
-      anchorElement.setAttribute("target", "_blank");
-    });
-    const col3AnchorElement1 = anchorElements[0];
-    const col3AnchorElement2 = anchorElements[1];
+    const col3AnchorElement1Href = anchorElements[0].getAttribute("href");
+    const col3AnchorElement2Href = anchorElements[1].getAttribute("href");
+    const col3AnchorElement1Text = anchorElements[0].textContent;
+    const col3AnchorElement2Text = anchorElements[1].textContent;
 
     //Fetch post date's text
     const postDateText = thirdColumn.querySelector("h6").textContent;
@@ -148,14 +148,14 @@ export default function decorate(block) {
                      <div class="col-md-7 d-none d-md-block aos-init aos-animate" data-aos="fade-up">
                         <div class="card border-0">
                            <div class="mb-2">
-                              ${secondColumnHeading}
+                              <h5 class="text-primary text-bold">${secondColumnHeading}</h5>
                            </div>
-                           ${secondColumnImage}
+                           <img class="card-border-color dummy-img" src="${secondColumnImage}" loading="lazy">
                            <div class="post mt-3 mb-3 csrbox">
                               <div class="post-body pb-3">
-                                 ${postTitle}
+                                 <h4 class="post-title">${postTitle}</h4>
                                  <p><span style="font-weight: normal;">${paraText}</span></p>
-                                 ${bottomAnchorElement2}
+                                 <a href="${bottomAnchorElement2Href}" class="link_view">${bottomAnchorElement2Text}</a>
                               </div>
                            </div>
                         </div>
@@ -163,25 +163,25 @@ export default function decorate(block) {
                      <div class="col-md-5 d-none d-md-block aos-init aos-animate" data-aos="fade-up">
                         <div class="card border-0 mb-lg-4">
                            <div class="mb-2">
-                              ${thirdColumnHeading1}
+                              <h5 class="text-primary text-bold">${thirdColumnHeading1}</h5>
                            </div>
                            <div class="post-body pb-3">
-                              ${image1}
-                              ${postTitleHeading1}
-                              ${col3AnchorElement1}
+                              <img class="card-border-color" alt="${image1Alt}" src="${image1Src}">
+                              <h4 class="post-title">${postTitleHeading1}</h4>
+                              <a href="${col3AnchorElement1Href}" class="link_view" target="_blank">${col3AnchorElement1Text}</a>
                            </div>
                         </div>
                         <div class="card border-0 mb-lg-4">
                            <div class="mb-2">
-                              ${thirdColumnHeading2}
+                              <h5 class="text-primary text-bold">${thirdColumnHeading2}</h5>
                            </div>
                            <div class="post-body pb-3">
-                              ${image2}
+                              <img class="card-border-color" alt="${image2Alt}" src="${image2Src}">
                               <div class="mb-2 mt-3">
                                  <span class="post-date text-primary">${postDateText}</span>
                               </div>
-                              ${postTitleHeading2}
-                              ${col3AnchorElement2}
+                              <h4 class="post-title">${postTitleHeading2}</h4>
+                              <a href="${col3AnchorElement2Href}" class="link_view" target="_blank">${col3AnchorElement2Text}</a>
                            </div>
                         </div>
                      </div>
@@ -194,16 +194,16 @@ export default function decorate(block) {
                                     <!-- product card -->
                                     <div class="card border-0">
                                        <div class="mb-2">
-                                          ${secondColumnHeading}
+                                          <h5 class="text-primary text-bold">${secondColumnHeading}</h5>
                                        </div>
                                        <div class="post-body pb-3">
                                           <div class="post-body pb-3">
-                                             ${secondColumnImage}
-                                             ${postTitle}
+                                             <img class="card-border-color" src="${secondColumnImage}" loading="lazy">
+                                             <h4 class="post-title">${postTitle}</h4>
                                              <h6 class="post-title five-lines ellipsis-double-line">
                                                 <p><span style="font-weight: normal;">${paraText}</span></p>
                                              </h6>
-                                             ${bottomAnchorElement2}
+                                             <a href="${col3AnchorElement1Href}" class="link_view">${bottomAnchorElement2Text}</a>
                                           </div>
                                        </div>
                                     </div>
@@ -213,12 +213,12 @@ export default function decorate(block) {
                                     <!-- product card -->
                                     <div class="card border-0">
                                        <div class="mb-2">
-                                          ${thirdColumnHeading1}
+                                          <h5 class="text-primary text-bold">${thirdColumnHeading1}</h5>
                                        </div>
                                        <div class="post-body pb-3">
-                                          ${image1}
-                                          ${postTitleHeading1}
-                                          ${col3AnchorElement1}
+                                          <img class="card-border-color" alt="${image1Alt}" src="${image1Src}">
+                                          <h4 class="post-title">${postTitleHeading1}</h4>
+                                          <a href="${col3AnchorElement1Href}" class="link_view" target="_blank">${col3AnchorElement1Text}</a>
                                        </div>
                                     </div>
                                     <!-- End -->
@@ -227,15 +227,15 @@ export default function decorate(block) {
                                     <!-- product card -->
                                     <div class="card border-0">
                                        <div class="mb-2">
-                                          ${thirdColumnHeading2}
+                                          <h5 class="text-primary text-bold">${thirdColumnHeading2}</h5>
                                        </div>
                                        <div class="post-body pb-3">
-                                          ${image2}
+                                          <img class="card-border-color" alt="${image2Alt}" src="${image2Src}">
                                           <div class="mb-2 mt-3">
                                              <span class="post-date text-primary">${postDateText}</span>
                                           </div>
-                                          ${postTitleHeading2}
-                                          ${col3AnchorElement2}
+                                          <h4 class="post-title">${postTitleHeading2}</h4>
+                                          <a href="${col3AnchorElement2Href}" class="link_view" target="_blank">${col3AnchorElement2Text}</a>
                                        </div>
                                     </div>
                                     <!-- End -->
