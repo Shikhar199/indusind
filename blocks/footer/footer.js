5f61d2b1c30e67ada2716fe5b0424c65d2e7bd80
Shubham Sharma <shubham.sharma@axeno.co>
2025-01-21 00:12:14 +0530

    Footer Block Update

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -21,15 +21,16 @@ export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  [...block.children].forEach((row) => {
+      const footerLinksContainer = row.children[1];
+      const officeDetailsContainer = row.children[2];
+      const footerBottomContainer = row.children[3];
+  });
+
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
   block.textContent = '';
-  [...block.children].forEach((row) => {
-    const footerLinksContainer = row.children[1];
-    const officeDetailsContainer = row.children[2];
-    const footerBottomContainer = row.children[3];
-  });
 
   decorateFooterLinks(footerLinksContainer);
   decorateOfficeDetails(officeDetailsContainer);
